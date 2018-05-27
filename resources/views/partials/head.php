66b6203417c0c899d033f96ff75d294124c7c343
Jimmy Murillo <salchichongallo1997@gmail.com>
2018-05-26 20:51:27 -0500

    Pasar JS tag a footer

--- resources/views/partials/head.php
+++ resources/views/partials/head.php
@@ -2,5 +2,4 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cárnicos</title>
-    <script src="./assets/bundle.js"></script>
 </head>
